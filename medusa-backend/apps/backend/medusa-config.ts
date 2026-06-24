dccae2b0282485440ec0be00689fb4cd27edd1d0
jeria88 <fjeriacastro@gmail.com>
2026-06-24 00:38:51 -0400

    Add Redis URL configuration to fix production deadlocks

--- medusa-backend/apps/backend/medusa-config.ts
+++ medusa-backend/apps/backend/medusa-config.ts
@@ -5,6 +5,7 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL || "redis://:ocJ93Qfmtgj76Hgjo8IaO832mMCGQQVn47jOmbS5u38n6fImuTo4Pt6DeUJlVB7W@q10ac16ubixriqizxg39govp:6379",
     http: {
       storeCors: process.env.STORE_CORS || "http://michicosas.146.181.39.4.sslip.io",
       adminCors: process.env.ADMIN_CORS || "http://michicosas-backend.146.181.39.4.sslip.io",
